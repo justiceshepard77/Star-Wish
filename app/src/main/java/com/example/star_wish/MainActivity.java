7799c69440007675c8293ac486bb56449731fac8
justiceshepard7 <59628984+justiceshepard7@users.noreply.github.com>
2021-12-01 13:08:44 -0800

    login working! email: test1@gmail.com password: password

--- app/src/main/java/com/example/star_wish/MainActivity.java
+++ app/src/main/java/com/example/star_wish/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.star_wish;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void login_user(View view) {
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+        String txtEmail = email.getText().toString();
+        String txtPassword = password.getText().toString();
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword(txtEmail , txtPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(MainActivity.this, "User name and " +
+                        "Password match", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this , MainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
